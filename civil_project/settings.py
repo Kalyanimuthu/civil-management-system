0b173f768f610eeade9f060a6c283cc3dff55f24
Kalyanimuthu <kalyaniram@gmail.com>
2026-02-18 19:40:24 +0530

    first commit

--- civil_project/settings.py
+++ civil_project/settings.py
@@ -89,16 +89,30 @@ WSGI_APPLICATION = 'civil_project.wsgi.application'
 # }
 
 
-import dj_database_url
 import os
+import dj_database_url
+from pathlib import Path
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    # Local development → SQLite
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
